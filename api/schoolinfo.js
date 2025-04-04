bed160577d4fc3a758eee8f6534ffa605fd6c1be
rewire7 <jinyang.kang@gmail.com>
2025-04-04 12:33:27 +0900

    Update schoolinfo.js

--- api/schoolinfo.js
+++ api/schoolinfo.js
@@ -1,16 +1,15 @@
 // 학교알리미 API 코드 업데이트 및 오류 처리 추가
 export default async function handler(req, res) {
-  const { school, level, region } = req.query;
+  const { school, level, region, year } = req.query;
 
-  const apiKey = "91464e07fb874c39b7a28ff2356d16b1";  // ✅ 여기에 API KEY를 넣으세요
-
-  if (!school || !level || !region) {
+  if (!school || !level || !region || !year) {
     return res.status(400).json({
-      error: '학교명, 학교급 코드, 시도교육청 코드를 모두 입력해주세요.'
+      error: '학교명, 학교급 코드, 시도교육청 코드, 연도 정보를 모두 입력해주세요.'
     });
   }
 
-  const url = `https://www.schoolinfo.go.kr/openApi.do?apiKey=${apiKey}&apiType=JSON&pbanScCode=${region}&schulKndCode=${level}&SCHUL_NM=${encodeURIComponent(school)}&pbanY=2024`;
+  const apiKey = '91464e07fb874c39b7a28ff2356d16b1';
+  const url = `https://www.schoolinfo.go.kr/openApi.do?apiKey=${apiKey}&apiType=JSON&pbanScCode=${region}&schulKndCode=${level}&SCHUL_NM=${encodeURIComponent(school)}&pbanY=${year}`;
 
   try {
     const response = await fetch(url);
